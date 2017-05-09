92dd1c35db457a95f2cb935aa493ce558129c47d
wangben0122 <1124979627@qq.com>
2017-05-09 10:45:39 +0800

    实体类

--- app/src/main/java/test/jiyun/com/myapplication/MainActivity.java
+++ app/src/main/java/test/jiyun/com/myapplication/MainActivity.java
@@ -2,12 +2,24 @@ package test.jiyun.com.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity implements View.OnClickListener {
+    private Button mBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //mdzz
+        mBtn = (Button) findViewById(R.id.Btn);
+        mBtn.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
