e920092788e4883d133c80a05a0a84f087f21032
wangben0122 <1124979627@qq.com>
2017-05-09 10:17:17 +0800

    实体类

---
+++ app/src/main/java/test/jiyun/com/myapplication/Student.java
@@ -0,0 +1,35 @@
+package test.jiyun.com.myapplication;
+
+/**
+ * Created by admin on 2017/5/9.
+ */
+
+public class Student {
+    private String name;
+    private int age;
+    private String sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
